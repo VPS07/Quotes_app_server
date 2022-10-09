3efc149b8f57450d6ddef476f7c57e5791cd0fdb
vps07 <vikysingh07860@gmail.com>
2022-10-09 23:43:30 +0530

    Made Some changes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const crudappRouter = require("./routes/crudapp");
 const port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(process.env.DB_URL)
